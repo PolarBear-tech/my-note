d23db186bf393063e4e682362efa9fa709b3fba5
Li Jinzhao <li.jinzhao.x@outlook.com>
2026-08-11 17:44:24 +0800

    vault backup: 2026-08-11 17:44:24

---
+++ Marimo/装饰器.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.23.16"
+app = marimo.App(css_file="")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
