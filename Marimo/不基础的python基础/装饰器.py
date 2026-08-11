0fc4accd85ac84cb7c1d014a26d4af241e70fc10
Li Jinzhao <li.jinzhao.x@outlook.com>
2026-08-11 18:24:51 +0800

    vault backup: 2026-08-11 18:24:51

--- Marimo/不基础的python基础/装饰器.py
+++ Marimo/不基础的python基础/装饰器.py
@@ -31,13 +31,13 @@ def _(mo):
 @app.cell
 def _(time):
     def timer(f):
-        def inner(*args, **kwargs):
+        def _inner(*args, **kwargs):
             start = time.time()
             f(*args, **kwargs)
             end = time.time()
             return end - start
 
-        return inner
+        return _inner
 
     return (timer,)
 
@@ -77,15 +77,53 @@ def _(mo):
     return
 
 
-app._unparsable_cell(
-    r"""
+@app.cell
+def _(time):
     def timer_with_arg(repeat):
-        def timer(f):
-            def inner():
-            
-    """,
-    name="_"
-)
+        def _timer(f):
+            def _inner(*args, **kwargs):
+                sum = 0
+                for i in range(repeat):
+                    start = time.time()
+                    f(*args, **kwargs)
+                    end = time.time()
+                    sum += end - start
+                    aver = sum / repeat
+                return aver
+            return _inner
+        return _timer
+
+    return (timer_with_arg,)
+
+
+@app.cell
+def _(time, timer_with_arg):
+    @timer_with_arg(10)
+    def gunc():
+        time.sleep(1)
+
+    return (gunc,)
+
+
+@app.cell
+def _(gunc):
+    gunc()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## 类与装饰器
+
+    类装饰器：
+    """)
+    return
+
+
+@app.cell
+def _():
+    return
 
 
 if __name__ == "__main__":
