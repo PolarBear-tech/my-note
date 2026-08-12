1b4750afbe0afc21742b322800f4077b97074a62
Li Jinzhao <li.jinzhao.x@outlook.com>
2026-08-12 11:12:50 +0800

    vault backup: 2026-08-12 11:12:50

--- Marimo/python高级语法/描述器.py
+++ Marimo/python高级语法/描述器.py
@@ -16,8 +16,53 @@ def _(mo):
     mo.md(r"""
     # 描述器
 
-    只要一个类定义了 `__get__` ``
+    只要一个类定义了 `__get__`、`__set__`或`__delete__`，它都会成为一个描述器
     """)
+    return
+
+
+@app.cell
+def _():
+    class Name:
+        def __get__(self, obj, objtype):
+            return "Peter"
+
+    class A:
+        name = Name()
+
+    _o = A()
+
+    print(_o.name)
+    print(A.name)
+    return (Name,)
+
+
+@app.cell
+def _(Name):
+    class B:
+        def __init__(self):
+            self.name = Name()
+
+    _o = B()
+    print(_o.name)
+    return
+
+
+@app.cell
+def _(Name):
+    class C:
+        name = Name()
+
+    _o = C()
+    _o.name = "Bob"
+    print(_o.name)
+    Name.__set__ = lambda x, y, z: None
+    print(_o.name)
+    return
+
+
+@app.cell
+def _():
     return
 
 
