04ee3749d5d0e74bd77c4a2ff98ca026eddb2ed9
Li Jinzhao <li.jinzhao.x@outlook.com>
2026-08-19 13:07:23 +0800

    vault backup: 2026-08-19 13:07:23

--- Marimo/python学习相关/functools.py
+++ Marimo/python学习相关/functools.py
@@ -8,9 +8,8 @@ app = marimo.App()
 def _():
     import marimo as mo
     import functools as ft
-    from timeit import timeit
 
-    return ft, mo, timeit
+    return ft, mo
 
 
 @app.cell
@@ -30,23 +29,87 @@ def _(mo):
 
 
 @app.cell
-def _(ft, timeit):
+def _(ft):
     @ft.lru_cache
     def _fib(n: int):
         if n <= 1:
             return n
         return _fib(n-1) + _fib(n - 2)
 
-    @timeit
-    def _main():
-        print(_fib(100))
+    _fib(50)
+    return
 
-    _main()
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## `partial`
+    """)
     return
 
 
 @app.cell
-def _():
+def _(ft):
+    def _pow(a: int, b: int) -> int:
+        return a ** b
+
+    _square = ft.partial(_pow, b=2)
+    _square(2)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## `reduce`
+    """)
+    return
+
+
+@app.cell
+def _(ft):
+    import operator
+
+    ft.reduce(operator.add, range(10))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## `cmp_to_key`
+    """)
+    return
+
+
+@app.cell
+def _(ft):
+    print(sorted([1,6,4,5,3], key=ft.cmp_to_key(lambda a, b: a - b)))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## `wraps`
+    """)
+    return
+
+
+@app.cell
+def _(ft):
+    def dec(func: callable):
+        @ft.wraps
+        def wrap(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrap
+
+    def func(a: int):
+        """
+        a: an integer
+        """
+        print(a)
+
     return
 
 
